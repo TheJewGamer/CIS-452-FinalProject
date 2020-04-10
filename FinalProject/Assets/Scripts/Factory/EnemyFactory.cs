33f9846957941d570d7ccaf0966f207c81a98909
KLong <60273128+krlongo@users.noreply.github.com>
2020-04-10 18:50:12 -0500

    Spawners and factory pattern is in

---
+++ FinalProject/Assets/Scripts/Factory/EnemyFactory.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Kevon Long
+ * MonsterFactory.cs
+ * Assignment #6
+ * determines which monster to spawn
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFactory : MonoBehaviour
+{
+    public GameObject EnemyPrefab;
+
+    private GameObject enemyToSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public GameObject CreateEnemy(string type)
+    {
+        Debug.Log("Creating " + type);
+        enemyToSpawn = null;
+
+        if (type.Equals("ExplodingEnemy"))
+        {
+            enemyToSpawn = EnemyPrefab;
+
+            if (enemyToSpawn.GetComponent<EnemyControllerSTD>() == null)
+            {
+                enemyToSpawn.AddComponent<EnemyControllerSTD>();
+            }
+        }
+            return enemyToSpawn;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ FinalProject/Assets/Scripts/Factory/EnemySpawner.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Kevon Long
+ * EnemySpawner.cs
+ * Final Project Part 2
+ * Spawns enemies using an object pooler and a factory pattern
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    public EnemyFactory factory;
+
+    private GameObject enemy;
+    public string type;
+    public float firerate;
+    private float nextFire;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > nextFire)
+        {
+            nextFire = Time.time + firerate;
+            type = "ExplodingEnemy";
+            enemy = factory.CreateEnemy(type);
+            Instantiate(enemy, gameObject.transform.position, gameObject.transform.rotation); //put the object pooler here instead of the instantiation
+        }
+    }
+
+}
