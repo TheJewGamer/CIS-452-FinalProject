a36104b0d8c74c3a00525b2c736d87f32d0fb92b
Warren <guileswa@gmail.com>
2020-04-10 19:26:43 -0500

    object pooler scripts
    these are the scripts from class

---
+++ FinalProject/Assets/Scripts/ObjectPooler/Pool.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+   /*
+	* Warren Guiles
+	 * Pool.cs
+	 * Team Covid
+	 * This is the pool class that is managed by the pooler.
+     I took this from the example code that you showed us.
+ */
+
+
+[System.Serializable]
+public class Pool
+{
+
+    //All of these need to be set in the inspector for each pool
+    public string tag;
+    public GameObject prefab;
+    public int size;
+    
+}
