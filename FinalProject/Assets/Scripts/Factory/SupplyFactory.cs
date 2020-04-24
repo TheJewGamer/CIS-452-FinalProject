6f99746fab4c7bddbe7fbbd458df3475af82ff27
CJ Green <55067215+Godofwar1300@users.noreply.github.com>
2020-04-24 11:31:55 -0500

    removed duplicate test scene of mine

---
+++ FinalProject/Assets/Scripts/Factory/SupplyFactory.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SupplyFactory : MonoBehaviour
+{
+
+    public GameObject supplyToSpawn;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject SpawnSupplies(string supplyType)
+    {
+
+        Debug.Log("Spawning" + supplyType);
+
+        supplyToSpawn = null;
+
+        supplyToSpawn = ObjectPooler.instance.SpawnFromPool(supplyType, Vector3.zero, Quaternion.identity);
+
+
+        return supplyToSpawn;
+    }
+}
